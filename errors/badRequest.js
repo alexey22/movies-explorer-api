52e90c997ab54d7725f2a0341c1572ec1bdd9f0c
Alexey Smirnov <alexey.smirnov.barnaul@yandex.ru>
2023-08-22 00:33:41 +0700

    delete comment from badRequest.js

--- errors/badRequest.js
+++ errors/badRequest.js
@@ -1,10 +1,6 @@
 module.exports = class BadRequest extends Error {
   constructor(err) {
     super(err);
-    // старая версия, которую не приняли
-    // if (err === 'Пользователь с таким email уже существует') {
-    //   this.statusCode = 409;
-    // } else this.statusCode = 400;
     this.statusCode = 400;
   }
 };
